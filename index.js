84a962e333e9f4a104ac5bc3195950e25807d46e
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-09-08 09:39:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+
+    
+const record = [
+    {year:'2015',result:'W'},
+    {year:'2014',result:'N/A'},
+    {year:'2013',result:'L'}]
+    
+
+
+function superbowlWin(array){
+ let result = array.find((object)=>object.result ==='W')
+ if(result === undefined){
+  return undefined
+ }else{
+ return result.year}
+}
+    // what is the difference between returning then console logging the result
+    // later on and console logging the result instead of returning it
+
+
+  console.log(superbowlWin(record))
+
+   //this is not possible
+   
